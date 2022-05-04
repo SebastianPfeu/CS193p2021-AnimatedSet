051bf8c79ac4db1bbea27ecb8b895e95479f6204
Sebastian Pfeufer <sebastianpfeufer@icloud.com>
2022-05-04 16:40:55 +0200

    Initial Commit

---
+++ AnimatedSet/AnimatedSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedSetApp.swift
+//  AnimatedSet
+//
+//  Created by Sebastian Pfeufer on 04.05.22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
